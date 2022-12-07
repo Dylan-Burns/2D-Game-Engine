c7e7ec9ddd5d1b1a4f5cf619ef165635eacaee9c
Burns <65887526+Dylan-Burns@users.noreply.github.com>
2022-12-07 13:00:02 -0800

    Add files via upload

---
+++ src/model/prize/FireFlower.java
@@ -0,0 +1,56 @@
+package model.prize;
+
+import manager.GameEngine;
+import model.hero.Mario;
+import model.hero.MarioForm;
+import view.Animation;
+import view.ImageLoader;
+
+import java.awt.image.BufferedImage;
+
+public class FireFlower extends BoostItem {
+
+    public FireFlower(double x, double y, BufferedImage style) {
+        super(x, y, style);
+        setPoint(150);
+    }
+
+    @Override
+    public void onTouch(Mario mario, GameEngine engine) {
+        mario.acquirePoints(getPoint());
+
+        ImageLoader imageLoader = new ImageLoader();
+
+        if(!mario.getMarioForm().isFire() && mario.getMarioForm().isSuper()){
+            BufferedImage[] leftFrames = imageLoader.getLeftFrames(MarioForm.FIRE);
+            BufferedImage[] rightFrames = imageLoader.getRightFrames(MarioForm.FIRE);
+
+            Animation animation = new Animation(leftFrames, rightFrames);
+            MarioForm newForm = new MarioForm(animation, true, true);
+            mario.setMarioForm(newForm);
+            mario.setDimension(48, 96);
+
+            engine.playFireFlower();
+        }
+        else {
+            BufferedImage[] leftFrames = imageLoader.getLeftFrames(MarioForm.SUPER);
+            BufferedImage[] rightFrames = imageLoader.getRightFrames(MarioForm.SUPER);
+
+            Animation animation = new Animation(leftFrames, rightFrames);
+            MarioForm newForm = new MarioForm(animation, true, false);
+            mario.setMarioForm(newForm);
+            mario.setDimension(48, 96);
+
+            engine.playFireFlower();
+        }
+    }
+
+    @Override
+    public void updateLocation(){}
+
+//    @Override
+//    public void onReveal(GameEngine engine) {
+//        engine.playPowerUpAppears();
+//    }
+
+}
